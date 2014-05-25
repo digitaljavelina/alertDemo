64bc4a9a878a5361318ca40dde23cd1a6e97a803
Michael Henry <mididoc@cox.net>
2014-05-25 07:34:16 -0700

    Initial Commit

---
+++ Alert/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Alert
+//
+//  Created by Michael Henry on 5/25/14.
+//  Copyright (c) 2014 Digital Javelina, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Alert/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Alert
+//
+//  Created by Michael Henry on 5/25/14.
+//  Copyright (c) 2014 Digital Javelina, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
